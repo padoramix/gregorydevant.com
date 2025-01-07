11a18c898ed0a62bde9dc42245e4cdd18befd493
Gregory DEVANT <gregory.devant@banque-edel.leclerc>
2025-01-07 17:50:12 +0100

    ADD base config file

---
+++ eleventy.config.js
@@ -0,0 +1,12 @@
+export default async function(eleventyConfig) {
+  // Access to the full `eleventyConfig` Configuration API
+
+};
+
+export const config = {
+  dir: {
+    input: "src",
+    output: "_site",
+    includes: "src/inc"
+  }
+};
