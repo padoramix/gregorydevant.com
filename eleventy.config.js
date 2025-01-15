6c9bcec126ecf95002b2f675c64dd396ffcc2fb0
Gregory DEVANT <gregory.devant@banque-edel.leclerc>
2025-01-15 16:19:03 +0100

    UPD Basic config not working

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,12 +1,12 @@
 export default async function(eleventyConfig) {
   // Access to the full `eleventyConfig` Configuration API
+  
 
 };
 
 export const config = {
   dir: {
     input: "src",
-    output: "_site",
-    includes: "src/inc"
+    includes: "_includes"
   }
 };
