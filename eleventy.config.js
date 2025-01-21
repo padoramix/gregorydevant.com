1a22385da23a20d203b73372c254f4c84e30015a
Gregory DEVANT <gregory.devant@banque-edel.leclerc>
2025-01-21 18:18:19 +0100

    UPD Change config to match new file organization

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,12 +1,18 @@
-export default async function(eleventyConfig) {
+export default async function (eleventyConfig) {
   // Access to the full `eleventyConfig` Configuration API
-  
-
-};
+  eleventyConfig.addPassthroughCopy({
+    "./public/": "/",
+  });
+}
 
 export const config = {
+  // Control which files Eleventy will process
+  templateFormats: ["md", "njk", "html", "liquid", "11ty.js"],
+
   dir: {
-    input: "src",
-    includes: "_includes"
-  }
+    input: "content",
+    includes: "../_includes",
+    data: "../_data",
+    output: "_site",
+  },
 };
